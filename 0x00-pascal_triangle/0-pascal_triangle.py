9da5d0c8a1c50c640f054b2269467588a573a467
twinnerhenock <demessiehe@gmail.com>
2023-01-26 12:43:06 +0000

    module for pascla's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""Computes value for the pascal triangle"""
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists of integers representing the Pascal's triangle of a given integer.
-    '''
-    triangle = []
+    """function for computing values of pascal triangle"""
+    pascals_triangle = []
+    counter = 1
     if n <= 0:
-        return triangle
-    for i in range(n):
-        a = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                a.append(1)
-            elif i > 0 and j > 0:
-                a.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(a)
-    return triangle
+        return pascals_triangle
+    if n == 1:
+        pascals_triangle.append([1])
+        return(pascals_triangle)
+    else:
+        pascals_triangle.append([1])
+        row_add = []
+        for row in pascals_triangle:
+            # Controling number of rows in traingle
+            if counter == n:
+                break
+            # Appending the first element in the row
+            # which is always 1
+            row_add.append(0 + row[0])
+            for i in range(0, len(row)):
+                # Adding elements in the last row of pascals_triangle
+                # and appending them to the new row, this is done for
+                # all middle elements
+                if (i != len(row) - 1):
+                    row_add.append(row[i] + row[i + 1])
+                else:
+                    # Appeding the last element (1) to the new row
+                    row_add.append(0 + row[len(row) - 1])
+            pascals_triangle.append(row_add)
+            row_add = []
+            counter = counter + 1
+        return pascals_triangle
